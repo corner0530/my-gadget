3007ec7388bf10bca76415286f05826613a286f9
corner0530 <2531yasuaki@gmail.com>
2024-04-23 02:24:06 +0900

    Add home index.html

--- font-changer/script.js
+++ font-changer/script.js
@@ -2,5 +2,4 @@ document.getElementById('textInput').addEventListener('input', function () {
   var text = this.value;
   document.getElementById('serifOutput').textContent = text;
   document.getElementById('sansSerifOutput').textContent = text;
-  document.getElementById('doublestruckOutput').textContent = text;
 });
